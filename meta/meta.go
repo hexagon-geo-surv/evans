e5592d59abeafd88fb721cfe761df674cb6413bd
Piotr321123 <piotrbelkner@gmail.com>
2025-08-13 01:21:20 +0200

    Updating version

--- meta/meta.go
+++ meta/meta.go
@@ -5,5 +5,5 @@ import version "github.com/hashicorp/go-version"
 const AppName = "evans"
 
 var (
-	Version = version.Must(version.NewSemver("0.10.11-leica"))
+	Version = version.Must(version.NewSemver("0.10.12-leica"))
 )
